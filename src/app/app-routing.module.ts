37d0bef050fa185952f86ef9a00e4e33ef97bb83
sonamrana11 <sonam.rana1110@gmail.com>
2021-06-08 18:40:54 +0530

    task is completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailsComponent } from './details/details.component';
+import { ErrorComponent } from './error/error.component';
+import { LayoutComponent } from './layout/layout.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '', redirectTo: 'layout/dashboard', pathMatch: 'full'
+  },
+  {
+    path: 'layout', component: LayoutComponent, children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'showdetails', component: DetailsComponent}
+    ]
+  },
+  { path: '**', component:ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FootbarComponent } from './footbar/footbar.component';
+import { DetailsComponent } from './details/details.component';
+import { LayoutComponent } from './layout/layout.component';
+import { ErrorComponent } from './error/error.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+    FootbarComponent,
+    DetailsComponent,
+    LayoutComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
